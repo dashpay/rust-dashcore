a1b1f2c54c6ab43d4ffc7ad60edd25a52f7e5f6f
shotonoff <dmitrii.golubev@dash.org>
2023-05-19 14:31:50 +0200

    fix: wip

--- contrib/release.sh
+++ contrib/release.sh
@@ -6,7 +6,7 @@
 set -ex
 
 main () {
-    for crate in "internals" "hashes" "bitcoin"; do
+    for crate in "internals" "hashes" "dash"; do
         if release_changes $crate; then
             echo "$crate has changes implying this is a release PR, checking if we can publish ..."
 
@@ -36,11 +36,11 @@ release_changes() {
 publish_dry_run() {
     local crate=$1
     if [ "$crate" == "hashes" ]; then
-        cargo publish -p "bitcoin_hashes" --dry-run
+        cargo publish -p "dashcore_hashes" --dry-run
     elif [ "$crate" == "internals" ]; then
-        cargo publish -p "bitcoin-private" --dry-run
-    elif [ "$crate" == "bitcoin" ]; then
-        cargo publish -p "bitcoin" --dry-run
+        cargo publish -p "dashcore-private" --dry-run
+    elif [ "$crate" == "dashcore" ]; then
+        cargo publish -p "dashcore" --dry-run
     fi
 }
 
