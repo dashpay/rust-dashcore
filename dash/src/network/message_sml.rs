d7a9c5888fb83c72e0090e1eacadab36490eeeae
Ivan Shumkov <ivan@shumkov.ru>
2025-02-13 09:49:34 +0700

    docs: add documentation for message fields

--- dash/src/network/message_sml.rs
+++ dash/src/network/message_sml.rs
@@ -6,36 +6,52 @@ use crate::transaction::special_transaction::quorum_commitment::QuorumFinalizati
 use crate::{BlockHash, ProTxHash, Transaction};
 use hashes::sha256::Hash;
 
-/// The getmnlistd message requests a mnlistdiff message that provides either:
-/// - A full masternode list (if baseBlockHash is all-zero)
+/// The `getmnlistd` message requests a `mnlistdiff` message that provides either:
+/// - A full masternode list (if `base_block_hash` is all-zero)
 /// - An update to a previously requested masternode list
 ///
 /// https://docs.dash.org/en/stable/docs/core/reference/p2p-network-data-messages.html#getmnlistd
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub struct GetMnListDiff {
+    /// Hash of a block the requester already has a valid masternode list of.
+    /// Note: Can be all-zero to indicate that a full masternode list is requested.
     pub base_block_hash: BlockHash,
+    /// Hash of the block for which the masternode list diff is requested
     pub block_hash: BlockHash,
 }
 
 impl_consensus_encoding!(GetMnListDiff, base_block_hash, block_hash);
 
-/// The mnlistdiff message is a reply to a getmnlistd message which requested
+/// The `mnlistdiff` message is a reply to a `getmnlistd` message which requested
 /// either a full masternode list or a diff for a range of blocks.
 ///
 /// https://docs.dash.org/en/stable/docs/core/reference/p2p-network-data-messages.html#mnlistdiff
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct MnListDiff {
+    /// Version of the message (currently 1).
+    /// In protocol versions 70225 through 70228 this field was located between the `coinbase_tx` and `deleted_masternodes` fields.
     pub version: u16,
+    /// Hash of a block the requester already has a valid masternode list of. Can be all-zero to indicate that a full masternode list is requested.
     pub base_block_hash: BlockHash,
+    /// Hash of the block for which the masternode list diff is requested
     pub block_hash: BlockHash,
+    /// Number of total transactions in `block_hash`
     pub total_transactions: u32,
+    /// Merkle hashes in depth-first order
     pub merkle_hashes: Vec<MerkleRootMasternodeList>,
+    /// Merkle flag bits, packed per 8 in a byte, least significant bit first
     pub merkle_flags: Vec<u8>,
+    /// The fully serialized coinbase transaction of blockHash
     pub coinbase_tx: Transaction,
+    /// A list of `ProRegTx` hashes for masternode which were deleted after `base_block_hash`
     pub deleted_masternodes: Vec<ProTxHash>,
+    /// The list of Simplified Masternode List (SML) entries which were added or updated since `base_block_hash`
     pub new_masternodes: Vec<MasternodeListEntry>,
+    /// A list of LLMQ type and quorum hashes for LLMQs which were deleted after `base_block_hash`
     pub deleted_quorums: Vec<DeletedQuorum>,
+    /// The list of LLMQ commitments for the LLMQs which were added since `base_block_hash`
     pub new_quorums: Vec<QuorumFinalizationCommitment>,
+    /// ChainLock signature used to calculate members per quorum indexes (in `new_quorums`)
     pub quorums_chainlock_signatures: Vec<BLSSignature>,
 }
 
@@ -65,11 +81,9 @@ pub struct DeletedQuorum {
 impl_consensus_encoding!(DeletedQuorum, llmq_type, quorum_hash);
 
 // TODO: Add encoding tests with test vectors from Dash Core
-// TODO: Add documentation
 
 #[cfg(test)]
 mod tests {
-
     use std::fs::File;
     use std::io::{self, Read};
     use assert_matches::assert_matches;
