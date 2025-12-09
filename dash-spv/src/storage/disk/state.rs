21db547d02781072884b6c291ffd3dab781e7537
Borja Castellano <borjacastellano1@gmail.com>
2025-12-09 21:50:46 +0000

    clippy warning fixed

--- dash-spv/src/storage/disk/state.rs
+++ dash-spv/src/storage/disk/state.rs
@@ -714,7 +714,8 @@ impl StorageManager for DiskStorageManager {
     }
 
     async fn shutdown(&mut self) -> StorageResult<()> {
-        Ok(Self::shutdown(self).await)
+        Self::shutdown(self).await;
+        Ok(())
     }
 }
 
