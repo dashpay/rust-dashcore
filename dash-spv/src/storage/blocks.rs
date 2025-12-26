87598584ee8bf5b7b57ff366969e69e44023b6eb
Borja Castellano <borjacastellano1@gmail.com>
2025-12-26 18:32:28 +0000

    general structure made

--- dash-spv/src/storage/blocks.rs
+++ dash-spv/src/storage/blocks.rs
@@ -2,8 +2,7 @@
 
 use std::collections::HashMap;
 use std::ops::Range;
-use std::path::Path;
-use std::sync::{Arc, RwLock};
+use std::path::PathBuf;
 
 use async_trait::async_trait;
 use dashcore::block::Header as BlockHeader;
@@ -48,27 +47,33 @@ pub trait BlockHeaderStorage {
 }
 
 pub struct PersistentBlockHeaderStorage {
-    block_headers: Arc<RwLock<SegmentCache<BlockHeader>>>,
-    header_hash_index: Arc<RwLock<HashMap<BlockHash, u32>>>,
+    block_headers: SegmentCache<BlockHeader>,
+    header_hash_index: HashMap<BlockHash, u32>,
+}
+
+impl PersistentBlockHeaderStorage {
+    const FOLDER_NAME: &str = "block_headers";
+    const INDEX_FILE_NAME: &str = "index.dat";
 }
 
 #[async_trait]
 impl PersistentStorage for PersistentBlockHeaderStorage {
-    async fn load(&self) -> StorageResult<Self> {
-        let index_path = self.base_path.join("headers/index.dat");
+    async fn load(storage_path: impl Into<PathBuf> + Send) -> StorageResult<Self> {
+        let storage_path = storage_path.into();
 
-        let block_headers = SegmentCache::load_or_new(base_path).await;
+        let index_path = storage_path.join(Self::FOLDER_NAME).join(Self::INDEX_FILE_NAME);
 
-        let header_hash_index = if let Ok(index) =
-            tokio::fs::read(&index_path).await.and_then(|content| bincode::deserialize(&content))
+        let block_headers = SegmentCache::load_or_new(storage_path).await?;
+
+        let header_hash_index = match tokio::fs::read(&index_path)
+            .await
+            .ok()
+            .map(|content| bincode::deserialize(&content).ok())
+            .flatten()
         {
-            index
-        } else {
-            block_headers.build_block_index_from_segments().await
+            Some(index) => index,
+            _ => block_headers.build_block_index_from_segments().await?,
         };
-
-        let block_headers = Arc::new(RwLock::new(block_headers));
-        let header_hash_index = Arc::new(RwLock::new(header_hash_index));
 
         Ok(Self {
             block_headers,
@@ -76,12 +81,15 @@ impl PersistentStorage for PersistentBlockHeaderStorage {
         })
     }
 
-    async fn persist(&self) {
-        let index_path = self.base_path.join("headers/index.dat");
+    async fn persist(&mut self, storage_path: impl Into<PathBuf> + Send) -> StorageResult<()> {
+        let block_headers_folder = storage_path.into().join(Self::FOLDER_NAME);
+        let index_path = block_headers_folder.join(Self::INDEX_FILE_NAME);
 
-        self.block_headers.write().await.persist().await;
+        tokio::fs::create_dir_all(block_headers_folder).await?;
 
-        let data = bincode::serialize(&self.header_hash_index.read().await)
+        self.block_headers.persist().await;
+
+        let data = bincode::serialize(&self.header_hash_index)
             .map_err(|e| StorageError::WriteFailed(format!("Failed to serialize index: {}", e)))?;
 
         atomic_write(&index_path, &data).await
@@ -91,7 +99,7 @@ impl PersistentStorage for PersistentBlockHeaderStorage {
 #[async_trait]
 impl BlockHeaderStorage for PersistentBlockHeaderStorage {
     async fn store_headers(&mut self, headers: &[BlockHeader]) -> StorageResult<()> {
-        let height = self.block_headers.read().await.next_height();
+        let height = self.block_headers.next_height();
         self.store_headers_at_height(headers, height).await
     }
 
@@ -104,13 +112,10 @@ impl BlockHeaderStorage for PersistentBlockHeaderStorage {
 
         let hashes = headers.iter().map(|header| header.block_hash()).collect::<Vec<_>>();
 
-        self.block_headers.write().await.store_items_at_height(headers, height).await?;
-
-        // Update reverse index
-        let mut reverse_index = self.header_hash_index.write().await;
+        self.block_headers.store_items_at_height(headers, height).await?;
 
         for hash in hashes {
-            reverse_index.insert(hash, height);
+            self.header_hash_index.insert(hash, height);
             height += 1;
         }
 
@@ -118,7 +123,7 @@ impl BlockHeaderStorage for PersistentBlockHeaderStorage {
     }
 
     async fn load_headers(&self, range: Range<u32>) -> StorageResult<Vec<BlockHeader>> {
-        self.block_headers.write().await.get_items(range).await
+        self.block_headers.get_items(range).await
     }
 
     async fn get_header(&self, height: u32) -> StorageResult<Option<BlockHeader>> {
@@ -138,26 +143,25 @@ impl BlockHeaderStorage for PersistentBlockHeaderStorage {
             return Ok(None);
         }
 
-        Ok(self.block_headers.write().await.get_items(height..height + 1).await?.first().copied())
+        Ok(self.load_headers(height..height + 1).await?.first().copied())
     }
 
     async fn get_tip_height(&self) -> Option<u32> {
-        self.block_headers.read().await.tip_height()
+        self.block_headers.tip_height()
     }
 
     async fn get_start_height(&self) -> Option<u32> {
-        self.block_headers.read().await.start_height()
+        self.block_headers.start_height()
     }
 
     async fn get_stored_headers_len(&self) -> u32 {
-        let headers_guard = self.block_headers.read().await;
-        let start_height = if let Some(start_height) = headers_guard.start_height() {
+        let start_height = if let Some(start_height) = self.block_headers.start_height() {
             start_height
         } else {
             return 0;
         };
 
-        let end_height = if let Some(end_height) = headers_guard.tip_height() {
+        let end_height = if let Some(end_height) = self.block_headers.tip_height() {
             end_height
         } else {
             return 0;
@@ -171,6 +175,6 @@ impl BlockHeaderStorage for PersistentBlockHeaderStorage {
         &self,
         hash: &dashcore::BlockHash,
     ) -> StorageResult<Option<u32>> {
-        Ok(self.header_hash_index.read().await.get(hash).copied())
+        Ok(self.header_hash_index.get(hash).copied())
     }
 }
--- dash-spv/src/storage/metadata.rs
+++ dash-spv/src/storage/metadata.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use async_trait::async_trait;
 
 use crate::{
@@ -12,29 +14,44 @@ pub trait MetadataStorage {
     async fn load_metadata(&self, key: &str) -> StorageResult<Option<Vec<u8>>>;
 }
 
-pub struct PersistentMetadataStorage {}
+pub struct PersistentMetadataStorage {
+    storage_path: PathBuf,
+}
+
+impl PersistentMetadataStorage {
+    const FOLDER_NAME: &str = "metadata";
+}
 
 #[async_trait]
 impl PersistentStorage for PersistentMetadataStorage {
-    async fn load(&self) -> StorageResult<Self> {
-        Ok(PersistentMetadataStorage {})
+    async fn load(storage_path: impl Into<PathBuf> + Send) -> StorageResult<Self> {
+        Ok(PersistentMetadataStorage {
+            storage_path: storage_path.into(),
+        })
     }
 
-    async fn persist(&self) {
+    async fn persist(&mut self, _storage_path: impl Into<PathBuf> + Send) -> StorageResult<()> {
         // Current implementation persists data everytime data is stored
+        Ok(())
     }
 }
 
 #[async_trait]
 impl MetadataStorage for PersistentMetadataStorage {
     async fn store_metadata(&mut self, key: &str, value: &[u8]) -> StorageResult<()> {
-        let path = self.base_path.join(format!("state/{}.dat", key));
+        let metadata_folder = self.storage_path.join(Self::FOLDER_NAME);
+        let path = metadata_folder.join(format!("{key}.dat"));
+
+        tokio::fs::create_dir_all(metadata_folder).await?;
+
         atomic_write(&path, value).await?;
+
         Ok(())
     }
 
     async fn load_metadata(&self, key: &str) -> StorageResult<Option<Vec<u8>>> {
-        let path = self.base_path.join(format!("state/{}.dat", key));
+        let path = self.storage_path.join(Self::FOLDER_NAME).join(format!("{key}.dat"));
+
         if !path.exists() {
             return Ok(None);
         }
