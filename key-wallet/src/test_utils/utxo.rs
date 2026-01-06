1a099887601326bb7eaef0ab82b2dd79fdb1fa94
Borja Castellano <borjacastellano1@gmail.com>
2026-01-06 16:13:16 +0000

    expose and used test_utils in itegration tests

--- key-wallet/src/test_utils/utxo.rs
+++ key-wallet/src/test_utils/utxo.rs
@@ -7,6 +7,22 @@ pub fn test_utxo(value: u64) -> Utxo {
     test_utxo_full(value, 100, 0, true)
 }
 
+pub fn test_utxo2(value: u64, confirmed: bool) -> Utxo {
+    let outpoint = OutPoint {
+        txid: Txid::from_raw_hash(sha256d::Hash::from_slice(&[1u8; 32]).unwrap()),
+        vout: 0,
+    };
+
+    let txout = TxOut {
+        value,
+        script_pubkey: ScriptBuf::new(),
+    };
+
+    let mut utxo = Utxo::new(outpoint, txout, test_address(), 100, false);
+    utxo.is_confirmed = confirmed;
+    utxo
+}
+
 pub fn test_utxo_full(value: u64, height: u32, vout: u32, confirmed: bool) -> Utxo {
     let outpoint = OutPoint {
         txid: Txid::from_raw_hash(sha256d::Hash::from_slice(&[1u8; 32]).unwrap()),
--- key-wallet/tests/test_optimal_consolidation.rs
+++ key-wallet/tests/test_optimal_consolidation.rs
@@ -1,47 +1,19 @@
+use key_wallet::test_utils::test_utxo2;
+
 // Test for OptimalConsolidation coin selection strategy
 #[test]
 fn test_optimal_consolidation_strategy() {
-    use dashcore::blockdata::script::ScriptBuf;
-    use dashcore::{Address, Network, OutPoint, TxOut, Txid};
-    use dashcore_hashes::{sha256d, Hash};
-    use key_wallet::utxo::Utxo;
     use key_wallet::wallet::managed_wallet_info::coin_selection::*;
     use key_wallet::wallet::managed_wallet_info::fee::FeeRate;
 
-    fn test_utxo(value: u64, confirmed: bool) -> Utxo {
-        let outpoint = OutPoint {
-            txid: Txid::from_raw_hash(sha256d::Hash::from_slice(&[1u8; 32]).unwrap()),
-            vout: 0,
-        };
-
-        let txout = TxOut {
-            value,
-            script_pubkey: ScriptBuf::new(),
-        };
-
-        let address = Address::p2pkh(
-            &dashcore::PublicKey::from_slice(&[
-                0x02, 0x50, 0x86, 0x3a, 0xd6, 0x4a, 0x87, 0xae, 0x8a, 0x2f, 0xe8, 0x3c, 0x1a, 0xf1,
-                0xa8, 0x40, 0x3c, 0xb5, 0x3f, 0x53, 0xe4, 0x86, 0xd8, 0x51, 0x1d, 0xad, 0x8a, 0x04,
-                0x88, 0x7e, 0x5b, 0x23, 0x52,
-            ])
-            .unwrap(),
-            Network::Testnet,
-        );
-
-        let mut utxo = Utxo::new(outpoint, txout, address, 100, false);
-        utxo.is_confirmed = confirmed;
-        utxo
-    }
-
     // Test that OptimalConsolidation strategy works correctly
     let utxos = vec![
-        test_utxo(100, true),
-        test_utxo(200, true),
-        test_utxo(300, true),
-        test_utxo(500, true),
-        test_utxo(1000, true),
-        test_utxo(2000, true),
+        test_utxo2(100, true),
+        test_utxo2(200, true),
+        test_utxo2(300, true),
+        test_utxo2(500, true),
+        test_utxo2(1000, true),
+        test_utxo2(2000, true),
     ];
 
     let selector = CoinSelector::new(SelectionStrategy::OptimalConsolidation);
