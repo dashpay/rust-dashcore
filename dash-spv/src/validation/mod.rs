7fe7a07a85975ced0b8da1384eb30f9970ce479f
Borja Castellano <borjacastellano1@gmail.com>
2026-01-19 04:12:13 -0800

    refactor: unify validation with a `Validator` trait (#355)

--- dash-spv/src/validation/mod.rs
+++ dash-spv/src/validation/mod.rs
@@ -1,5 +1,13 @@
 //! Validation functionality for the Dash SPV client.
 
+mod header;
 mod instantlock;
 
+pub use header::BlockHeaderValidator;
 pub use instantlock::InstantLockValidator;
+
+use crate::error::ValidationResult;
+
+pub trait Validator<T> {
+    fn validate(&self, data: T) -> ValidationResult<()>;
+}
