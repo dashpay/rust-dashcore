fe9b2e9d846f3108b2a93cf09351b3fb1eb8ba8f
Borja Castellano <borjacastellano1@gmail.com>
2026-03-31 17:04:38 +0000

    .c files to validate headers and script to execute them

---
+++ ffi-c-tests/header-tests/dash-spv.c
@@ -0,0 +1,6 @@
+#include "../../target/debug/include/dash-spv-ffi/dash-spv-ffi.h"
+
+int main() {
+
+    return 0;
+}
---
+++ ffi-c-tests/header-tests/spv_and_key_wallet.c
@@ -0,0 +1,7 @@
+#include "../../target/debug/include/dash-spv-ffi/dash-spv-ffi.h"
+#include "../../target/debug/include/key-wallet-ffi/key-wallet-ffi.h"
+
+int main() {
+
+    return 0;
+}
