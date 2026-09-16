4743889feed1eda9190e336132a8653ca9c309b8
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-09-16 16:19:50 +0530

    refactor(dash)!: drop the unconstructed Ed25519 key error variants

--- dash/src/base58.rs
+++ dash/src/base58.rs
@@ -52,10 +52,6 @@ pub enum Error {
     // TODO: Remove this as part of crate-smashing, there should not be any key related errors in this module
     Hex(hex::Error),
 
-    /// edwards 25519 related error
-    #[cfg(feature = "ed25519-dalek")]
-    Ed25519Dalek(String),
-
     /// A feature is not supported
     NotSupported(String),
 }
@@ -77,8 +73,6 @@ impl fmt::Display for Error {
             Error::TooShort(_) => write!(f, "base58ck data not even long enough for a checksum"),
             Error::Secp256k1(ref e) => fmt::Display::fmt(&e, f),
             Error::Hex(ref e) => write!(f, "Hexadecimal decoding error: {}", e),
-            #[cfg(feature = "ed25519-dalek")]
-            Error::Ed25519Dalek(ref e) => write!(f, "Ed25519-Dalek error: {}", e),
             Error::NotSupported(ref e) => write!(f, "Not supported: {}", e),
         }
     }
@@ -416,8 +410,6 @@ impl From<key::Error> for Error {
             key::Error::InvalidKeyPrefix(_) => Error::Secp256k1(secp256k1::Error::InvalidPublicKey),
             key::Error::Hex(e) => Error::Hex(e),
             key::Error::InvalidHexLength(size) => Error::InvalidLength(size),
-            #[cfg(feature = "ed25519-dalek")]
-            key::Error::Ed25519Dalek(e) => Error::Ed25519Dalek(e),
             key::Error::NotSupported(e) => Error::NotSupported(e),
         }
     }
