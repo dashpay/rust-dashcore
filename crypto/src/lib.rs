e9fd825f463f1d9b41ed314c33ee0e6b737a0e6f
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-09-18 20:55:17 +0530

    feat: introduce the `dashcore-crypto` crate, migrate BLS crypto
    `git diff --color-moved=dimmed-zebra --color-moved-ws=ignore-all-space`

---
+++ crypto/src/lib.rs
@@ -0,0 +1,18 @@
+//
+// This file is a part of rust-dashcore.
+// SPDX-License-Identifier: CC0-1.0
+// See the accompanying file LICENSE or https://creativecommons.org/publicdomain/zero/1.0
+//
+
+//! Cryptographic primitives shared by rust-dashcore crates
+
+#![doc(html_logo_url = "https://media.dash.org/wp-content/uploads/dash-d-logo.svg")]
+#![doc(html_favicon_url = "https://media.dash.org/wp-content/uploads/dash-d-logo.svg")]
+
+extern crate alloc;
+
+pub extern crate dashcore_hashes as hashes;
+#[cfg(feature = "serde")]
+pub extern crate serde;
+
+pub mod bls;
