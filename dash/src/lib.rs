210f634091d3bdc27287c49902375abe28b7f7eb
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-09-24 17:39:21 +0530

    refactor(dash): reap `platform_node_id`, retain as `EddsaPkHash` alias

--- dash/src/lib.rs
+++ dash/src/lib.rs
@@ -105,7 +105,9 @@ pub mod ephemerealdata;
 pub mod error;
 pub mod hash_types;
 pub mod merkle_tree;
-pub mod platform_node_id;
+pub mod platform_node_id {
+    pub use dashcore_crypto::eddsa::EddsaPkHash as PlatformNodeId;
+}
 pub mod policy;
 pub mod pow;
 pub mod sign_message;
